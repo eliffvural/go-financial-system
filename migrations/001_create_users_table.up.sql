a8b39f9fd4b92b0da444079067b2650d68e45f97
Elif Vural <elifvural019@gmail.com>
2025-07-25 21:29:56 +0300

    migrations added

---
+++ migrations/001_create_users_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+); 
